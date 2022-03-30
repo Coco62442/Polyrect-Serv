6237343c57f63b046fd91c6246be21b8deb8c35c
Coco62442 <corentinclement10@gmail.com>
2022-03-30 19:49:45 +0200

    Correctifs

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-// const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 
